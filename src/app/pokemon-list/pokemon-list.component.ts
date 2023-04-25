2009b19f913ce012990f59788252ce1b3097f2c2
kevin.caires <kevin.caires@smartgreen.net>
2023-04-25 17:17:18 -0300

    Inicia uma propriedade para listagem de pokemons.

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/model/pokemon';
+import { Type } from 'src/model/Type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent {
 
+export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ]
+    }
+  ];
 }
